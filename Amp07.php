ce99ebfd1bae6ff80efa58648a047ff2c2569a9e
SrGio <sergionavarronavarro@msn.com>
2015-12-02 13:03:38 +0100

    Probando Git

---
+++ Amp07.php
@@ -0,0 +1,17 @@
+<?php
+	for ($i=0; $i<=6; $i++)
+		switch ($i) {
+		case 0:
+		case 1:
+			break;
+		case 2:
+			echo "1";
+		case 3:
+			break;
+		case 5:
+			echo "5";
+			break;
+		default:
+			echo "d";
+		}
+?>
